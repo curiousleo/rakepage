aaba05c7463c9269cd73206980f7ef1c6fa44f0a
curiousleo <curiousleo@ymail.com>
2011-04-23 21:05:37 +0100

    Massive changes

--- dodo.py
+++ dodo.py
@@ -4,12 +4,68 @@
 import os
 import os.path
 from contextlib import closing
+import yaml
 
-conf = '''
-input:      {enc: utf-8, ext: .textile}
-options:    {conf: _site.yaml, preview: true, template: _template.html}
-output:     {dir: ., enc: utf-8, ext: .html, type: local}
+DEFAULT_CONF = '''
+input:
+    enc: utf-8
+    ext: .textile
+options:
+    preview: true
+output:
+    enc: utf-8
+    ext: .html
+    type: local
 '''
+CONF = yaml.load(DEFAULT_CONF)
+
+PAGE_DIR = 'pages'
+OUTPUT_DIR = 'output'
+PAGES = []
+SRC_FILES = []
+DST_FILES = []
+
+def get_pages():
+    for page in CONF['sitemap']:
+        name, title = page.items()[0]
+        # yield {'title': title, 'path': name}
+        PAGES.append({'title': title, 'path': name})
+
+def get_dst_files():
+    for page in PAGES:
+        # yield os.path.join(
+            # conf['output']['dir'], page['name'] + conf['output']['ext'])
+        DST_FILES.append(os.path.join(
+            OUTPUT_DIR, page['name'] + CONF['output']['ext']))
+
+def get_src_files():
+    for page in PAGES:
+        # yield os.path.join(
+            # conf['input']['dir'], page['name'] + conf['input']['ext'])
+        DST_FILES.append(os.path.join(
+            PAGE_DIR, page['name'] + CONF['input']['ext']))
+
+def load_conf(confpath):
+    def deep_merge(dst, src):
+        '''
+        Merges src into dst, returns dst.
+        Credits: Manuel Muradas <http://bit.ly/fOPgyW>
+        '''
+        stack = [(dst, src)]
+        while stack:
+            current_dst, current_src = stack.pop()
+            for key in current_src:
+                if key not in current_dst: current_dst[key] = current_src[key]
+                else:
+                    if isinstance(current_src[key], dict) and \
+                        isinstance(current_dst[key], dict):
+                        stack.append((current_dst[key], current_src[key]))
+                    else:
+                        current_dst[key] = current_src[key]
+        return dst
+
+    with closing(open(confpath)) as confpath:
+        deep_merge(CONF, yaml.load(confpath))
 
 def task_create():
     """
@@ -17,14 +73,15 @@ def task_create():
     """
     
     new_site = {
-        'config.yaml': conf,
+        'config.yaml': DEFAULT_CONF,
         'pages':
             {'index.textile': ''},
         'media':
             {'default.css': ''},
         'templates':
             {'default.mustache': ''},
-        'site': {}
+        'site': {},
+        'output': {},
     }
 
 
@@ -63,13 +120,14 @@ def task_create():
                     if not os.path.exists(target): os.mkdir(target)
                     write_defaults(content, target)
 
-    return {'actions': [(write_defaults,)],
+    yield  {'name': 'create',
+            'actions': [(write_defaults,)],
             'targets': get_targets(),
             # 'params':[{'name': 'layout',
                        # 'short': 'l',
                        # 'default': 'basic'}],
-            'clean': ['rm -rf %s' % ' '.join(get_targets())],
-            'run_once': True
+            'run_once': True,
+            'clean': ['rm -rf %s' % 'output']
             }
 
 def task_gen():
@@ -77,11 +135,12 @@ def task_gen():
     Generate a site.
     """
 
-    return {'actions': [],
-            'targets': [],
-            # 'params':[{'name': 'layout',
-                       # 'short': 'l',
-                       # 'default': 'basic'}],
+    return {'actions': [(load_conf, get_pages, get_src_files, get_dst_files)],
+            'targets': ['test'],
+            'params':[{'name': 'confpath',
+                       'short': 'c',
+                       'long': 'conf',
+                       'default': CONF['options']['conf']}],
             }
 
 # vim: set fileencoding=utf8 expandtab tabstop=4 shiftwidth=4 softtabstop=4:
