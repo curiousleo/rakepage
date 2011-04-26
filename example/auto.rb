bed04729c842797311227e8d27218efc757d8120
curiousleo <curiousleo@ymail.com>
2011-04-26 17:54:27 +0100

    Removed auto.rb -> is included in the Rakefile now

--- example/auto.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'watchr'
-
-# all_rb = Dir['*.rb'].join('|')
-script = Watchr::Script.new
-script.watch('Rakefile') { |file| system("rake") }
-controller = Watchr::Controller.new(script, Watchr.handler.new)
-controller.run
